a0b7f5eaf70bf0e14230a57d6deeb90654686461
Mikhail Yakushin <driver733@me.com>
2016-01-09 22:13:11 -0500

    fixed vk auth callback

--- VKStorage/AppDelegate.swift
+++ VKStorage/AppDelegate.swift
@@ -53,6 +53,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
   func applicationWillTerminate(application: UIApplication) {
     // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
   }
+  
+  func application(app: UIApplication, openURL url: NSURL, options: [String : AnyObject]) -> Bool {
+    VKSdk.processOpenURL(url, fromApplication: url.URLString)
+    return true
+  }
 
 
 }
