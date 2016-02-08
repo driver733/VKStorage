be445f600241afe959e7b440fe56c569bc2ea089
biboran <tim-solonin2@yandex.ru>
2016-02-08 12:56:59 -0500

    getDocs()

--- VKStorage/SearchQuery.swift
+++ VKStorage/SearchQuery.swift
@@ -10,7 +10,7 @@ import Foundation
 
 class SearchQuery {
   
-  private let docs = CurrentUser.sharedCurrentUser().documentArray.documents
+  private var docs = CurrentUser.sharedCurrentUser().documentArray.documents
   
   private let documentTypes = [
     "Documents"   : ["pdf"],
@@ -20,22 +20,23 @@ class SearchQuery {
     "Other"       : []
   ]
   
-  private let extentionsName = "Extentions"
-  private let datesName      = "Dates"
-  private let typesName      = "Types"
+  private let extentionsName = "Extentions:"
+  private let datesName      = "Dates:"
+  private let typesName      = "Types:"
+  private let beginsWithName = "Begins with:"
+  private let endsWithName   = "Ends with:"
   
-  private var configurations : [SearchConfig]
-  private var configurationsNames : [String] {
-   return Array<String>(configurations.map() { $0.name })
-  }
+  private var configurations : [String : [SearchConfig]]
   
   init() {
-//    configurations = [
-//      extentionsName : [String](),
-//      datesName      : [String](),
-//      typesName      : [String]()
-//    ]
-    configurations = [SearchConfig]()
+    configurations = [
+      extentionsName : [SearchConfig](),
+      datesName      : [SearchConfig](),
+      typesName      : [SearchConfig](),
+      beginsWithName : [SearchConfig](),
+      endsWithName   : [SearchConfig]()
+    ]
+//    configurations = [SearchConfig]()
   }
   
   //Implement dispatch_async?
@@ -49,7 +50,6 @@ class SearchQuery {
     var suggestions = [(String, [SearchConfig])]()
     
     extentionsStartingWith(suggest).continueWithBlock { (task: BFTask) -> AnyObject? in
-      
       if let a = task.result as? [SearchConfig] {
         suggestions.append(self.extentionsName, a)
       }
@@ -70,8 +70,8 @@ class SearchQuery {
       return nil
     }
     
-//    objects["Title begins with:"] = [suggest]
-//    objects["Title ends with:"] = [suggest]
+    suggestions.append((beginsWithName, [SearchConfig(name: suggest, type: .Beginning)]))
+    suggestions.append((endsWithName,   [SearchConfig(name: suggest, type: .Ending)]))
     
     return suggestions
   }
@@ -83,12 +83,19 @@ class SearchQuery {
     
     let filteredDocs = docs.filter() { $0.ext.lowercaseString.hasPrefix(str.lowercaseString) }
     for doc in filteredDocs {
-      if !configurationsNames.contains(doc.ext) {
+      if !namesArray(configurations[extentionsName]!).contains(doc.ext) {
         extentions.insert(SearchConfig(name: doc.ext, type: .Extention))
       }
     }
-
-    task.setResult(Array<SearchConfig>(extentions))
+    
+    let result = Array<SearchConfig>(extentions)
+    print(result.first?.name)
+    if !result.isEmpty {
+      task.setResult(result)
+    }
+    else {
+      task.setResult(nil)
+    }
   
     return task.task
   }
@@ -98,19 +105,23 @@ class SearchQuery {
     
     let dateFormatter = NSDateFormatter()
     dateFormatter.dateFormat = "MMMM yyyy"
-    let sdateFormatter = NSDateFormatter()
-    sdateFormatter.dateFormat = "MM yyyy"
     
     var stringDates = Set<SearchConfig>()
     
     let filteredDocs = docs.filter() { dateFormatter.stringFromDate($0.date).lowercaseString.hasPrefix(str.lowercaseString) }
     for doc in filteredDocs {
-      if !configurationsNames.contains(dateFormatter.stringFromDate(doc.date)) {
-        stringDates.insert(SearchConfig(name: dateFormatter.stringFromDate(doc.date), shortName: sdateFormatter.stringFromDate(doc.date), type: .Date))
+      if !namesArray(configurations[datesName]!).contains(dateFormatter.stringFromDate(doc.date)) {
+        stringDates.insert(SearchConfig(name: dateFormatter.stringFromDate(doc.date), type: .Date))
       }
     }
     
-    task.setResult(Array<SearchConfig>(stringDates))
+    let result = Array<SearchConfig>(stringDates)
+    if !result.isEmpty {
+      task.setResult(result)
+    }
+    else {
+      task.setResult(nil)
+    }
     
     return task.task
   }
@@ -120,13 +131,19 @@ class SearchQuery {
     
     var filteredTypes = Array<String>(documentTypes.keys).filter() { $0.lowercaseString.hasPrefix(str.lowercaseString) }
     
-    for config in configurationsNames {
+    for config in (namesArray(configurations[typesName]!)) {
       if let index = filteredTypes.indexOf(config) {
         filteredTypes.removeAtIndex(index)
       }
     }
-    let types = Array<SearchConfig>(filteredTypes.map() { SearchConfig(name: $0, type: .Type) })
-    task.setResult(types)
+    
+    let result = Array<SearchConfig>(filteredTypes.map() { SearchConfig(name: $0, type: .Type) })
+    if !result.isEmpty {
+      task.setResult(result)
+    }
+    else {
+      task.setResult(nil)
+    }
     
     return task.task
   }
@@ -146,11 +163,9 @@ class SearchQuery {
         
         let dateFormatter = NSDateFormatter()
         dateFormatter.dateFormat = "MMMM yyyy"
-        let sdateFormatter = NSDateFormatter()
-        sdateFormatter.dateFormat = "MM yyyy"
         
         for doc in docs {
-          datesSet.insert(SearchConfig(name: dateFormatter.stringFromDate(doc.date), shortName: sdateFormatter.stringFromDate(doc.date), type: .Date))
+          datesSet.insert(SearchConfig(name: dateFormatter.stringFromDate(doc.date), type: .Date))
         }
         return Array<SearchConfig>(datesSet)
       }())
@@ -159,10 +174,65 @@ class SearchQuery {
   }
   
   func addConfiguration(conf: SearchConfig) -> Bool {
-    configurations.append(conf)
+    switch conf.type {
+    case .Extention:
+      configurations[extentionsName]?.append(conf)
+    case .Type:
+      configurations[typesName]?.append(conf)
+    case .Date:
+      configurations[datesName]?.append(conf)
+    case .Beginning:
+      configurations[beginsWithName]?.append(conf)
+    case .Ending:
+      configurations[endsWithName]?.append(conf)
+//    default:
+//      return false
+    }
     return true
   }
   
+  func getDocs() -> [Document] {
+    var docSet = Set<Document>()
+    for key in configurations.keys {
+//      let b = configurations[key]?.filter() { $0. }
+      for config in configurations[key]! {
+        switch config.type {
+        case .Beginning:
+          let result = docs.filter() { $0.title.lowercaseString.hasPrefix(config.name.lowercaseString) }
+          for i in result {
+            docSet.insert(i)
+          }
+        case .Ending:
+          let result = docs.filter() { $0.title.lowercaseString.hasSuffix(config.name.lowercaseString) }
+          for i in result {
+            docSet.insert(i)
+          }
+        case .Extention:
+          let result = docs.filter() { $0.ext.lowercaseString == config.name.lowercaseString }
+          for i in result {
+            docSet.insert(i)
+          }
+        case .Date:
+          let dateFormatter = NSDateFormatter()
+          dateFormatter.dateFormat = "MMMM yyyy"
+          let result = docs.filter() { dateFormatter.stringFromDate($0.date).lowercaseString == config.name.lowercaseString }
+          for i in result {
+            docSet.insert(i)
+          }
+        case .Type:
+          let result = docs.filter() { documentTypes[config.name]!.contains($0.ext.lowercaseString) }
+          for i in result {
+            docSet.insert(i)
+          }
+        }
+      }
+    }
+    return Array<Document>(docSet)
+  }
+  
+  func namesArray(source: [SearchConfig]) -> [String] {
+    return source.map() { $0.name }
+  }
   
 }
 
@@ -170,6 +240,8 @@ enum SearchConfigType {
   case Date
   case Type
   case Extention
+  case Beginning
+  case Ending
 }
 
 class SearchConfig: AnyObject, Hashable {
@@ -179,18 +251,26 @@ class SearchConfig: AnyObject, Hashable {
   var type      : SearchConfigType
   
   var hashValue : Int {
-      return name.hashValue
+    return name.hashValue
   }
   
-  init(name: String, shortName: String, type: SearchConfigType) {
-    self.name      = name
-    self.shortName = shortName
-    self.type      = type
-  }
+//  init(name: String, shortName: String, type: SearchConfigType) {
+//    self.name      = name
+//    self.shortName = shortName
+//    self.type      = type
+//  }
   
   init(name: String, type: SearchConfigType) {
     self.name      = name
     self.type      = type
+    
+    if type == .Date {
+      let sdateFormatter = NSDateFormatter()
+      sdateFormatter.dateFormat = "MMM yyyy"
+      if let date = sdateFormatter.dateFromString(name) {
+        shortName = sdateFormatter.stringFromDate(date)
+      }
+    }
   }
   
 }
