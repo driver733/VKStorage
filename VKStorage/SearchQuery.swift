9d05159b6103bb3a131f4ccffa0c0d9e02a69b12
biboran <tim-solonin2@yandex.ru>
2016-02-07 19:30:10 -0500

    tuple return

--- VKStorage/SearchQuery.swift
+++ VKStorage/SearchQuery.swift
@@ -20,42 +20,42 @@ class SearchQuery {
     "Other"       : []
   ]
   
-  private let extentionsIndex = "Extentions"
-  private let datesIndex      = "Dates"
-  private let typesIndex      = "Types"
+  private let extentionsName = "Extentions"
+  private let datesName      = "Dates"
+  private let typesName      = "Types"
   
   private var configurations: [String: [String]]
   
   init() {
     configurations = [
-      extentionsIndex : [String](),
-      datesIndex      : [String](),
-      typesIndex      : [String]()
+      extentionsName : [String](),
+      datesName      : [String](),
+      typesName      : [String]()
     ]
   }
   
   //Implement dispatch_async?
-  func suggestConfiguration(suggest: String) -> [String : [String]] {
+  func suggestConfiguration(suggest: String) -> [(String, [String]?)] {
     
     if suggest.isEmpty {
       //Default return
       return defaultSuggest()
     }
     
-    var suggestions = [String : [String]]()
+    var suggestions = [(String, [String]?)]()
     
     extentionsStartingWith(suggest).continueWithBlock { (task: BFTask) -> AnyObject? in
-      suggestions[self.extentionsIndex] = task.result as? [String]
+      suggestions.append(self.extentionsName, task.result as? [String])
       return nil
     }
     
     datesStartingWith(suggest).continueWithBlock { (task: BFTask) -> AnyObject? in
-      suggestions[self.datesIndex] = task.result as? [String]
+      suggestions.append(self.datesName, task.result as? [String])
       return nil
     }
     
     typesStartingWith(suggest).continueWithBlock { (task: BFTask) -> AnyObject? in
-      suggestions[self.typesIndex] = task.result as? [String]
+      suggestions.append(self.typesName, task.result as? [String])
       return nil
     }
     
@@ -72,7 +72,7 @@ class SearchQuery {
     
       let filteredDocs = docs.filter() { $0.ext.hasPrefix(str) }
       for doc in filteredDocs {
-        if !configurations[self.extentionsIndex]!.contains(doc.ext) {
+        if !configurations[self.extentionsName]!.contains(doc.ext) {
           extentions.append(doc.ext)
         }
       }
@@ -90,7 +90,7 @@ class SearchQuery {
     var stringDates = [String]()
     let filteredDocs = docs.filter() { dateFormatter.stringFromDate($0.date).hasPrefix(str) }
     for doc in filteredDocs {
-      if !configurations[self.datesIndex]!.contains(dateFormatter.stringFromDate(doc.date)) {
+      if !configurations[self.datesName]!.contains(dateFormatter.stringFromDate(doc.date)) {
         stringDates.append(dateFormatter.stringFromDate(doc.date))
       }
     }
@@ -105,7 +105,7 @@ class SearchQuery {
     
     var filteredTypes = Array<String>(documentTypes.keys).filter() { $0.hasPrefix(str) }
     
-    for config in configurations[self.typesIndex]! {
+    for config in configurations[self.typesName]! {
       if let index = filteredTypes.indexOf(config) {
         filteredTypes.removeAtIndex(index)
       }
@@ -125,16 +125,16 @@ class SearchQuery {
     return unique
   }
   
-  func defaultSuggest() -> [String: [String]] {
+  func defaultSuggest() -> [(String, [String]?)] {
     return [
-      self.typesIndex      : Array<String>(documentTypes.keys),
-      self.extentionsIndex : {
+      (self.typesName      , Array<String>(documentTypes.keys)),
+      (self.extentionsName , {
         var extentionSet = Set<String>()
         for doc in docs {
           extentionSet.insert(doc.ext)
         }
         return Array<String>(extentionSet)
-        }()
+        }())
     ]
   }
   
